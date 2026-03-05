e904718b4f042af2efd7c700a50566596e6bed02
CorentinCLERO <corentinclero@hotmail.com>
2026-03-05 15:03:54 +0100

    feat: enhance tests for poker hand comparison with additional cards and assertions

--- index.test.ts
+++ index.test.ts
@@ -24,11 +24,72 @@ test("it return 5 cards", () => {
       rank: Rank.Three,
       color: Color.Spades,
     },
+    {
+      rank: Rank.Four,
+      color: Color.Spades,
+    },
+    {
+      rank: Rank.Five,
+      color: Color.Spades,
+    },
+    {
+      rank: Rank.Six,
+      color: Color.Spades,
+    },
   ];
 
   // When
-  const expected_result = getBestHand(user_cards, community_cards);
+  const result = getBestHand(user_cards, community_cards);
 
   // Then
-  expect(expected_result.length).toBe(5);
+  expect(result.length).toBe(5);
+});
+
+test("it return 5 highest cards", () => {
+  // Given
+  const user_cards: Card[] = [
+    {
+      rank: Rank.Two,
+      color: Color.Spades,
+    },
+    {
+      rank: Rank.Jack,
+      color: Color.Spades,
+    },
+  ];
+
+  const community_cards: Card[] = [
+    {
+      rank: Rank.Six,
+      color: Color.Spades,
+    },
+    {
+      rank: Rank.Three,
+      color: Color.Spades,
+    },
+    {
+      rank: Rank.Four,
+      color: Color.Spades,
+    },
+    {
+      rank: Rank.Five,
+      color: Color.Spades,
+    },
+    {
+      rank: Rank.Seven,
+      color: Color.Spades,
+    },
+  ];
+
+  // When
+  const result = getBestHand(user_cards, community_cards);
+
+  // Then
+  expect(result.map((card) => card.rank)).toBe([
+    Rank.Jack,
+    Rank.Seven,
+    Rank.Six,
+    Rank.Five,
+    Rank.Four,
+  ]);
 });
