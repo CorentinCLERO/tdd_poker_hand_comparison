ea88167572b4559674b377bb66a756c787224020
CorentinCLERO <corentinclero@hotmail.com>
2026-03-05 15:16:07 +0100

    feat: add tests for card count validation and highest card selection in getBestHand function

--- index.test.ts
+++ index.test.ts
@@ -45,6 +45,111 @@ test("it return 5 cards", () => {
   expect(result.length).toBe(5);
 });
 
+test("user should have 2 cards", () => {
+  // Given
+  const user_cards: Card[] = [];
+
+  const community_cards: Card[] = [
+    {
+      rank: Rank.Two,
+      color: Color.Spades,
+    },
+    {
+      rank: Rank.Three,
+      color: Color.Spades,
+    },
+    {
+      rank: Rank.Four,
+      color: Color.Spades,
+    },
+    {
+      rank: Rank.Five,
+      color: Color.Spades,
+    },
+    {
+      rank: Rank.Six,
+      color: Color.Spades,
+    },
+  ];
+
+  // When
+  // Then
+  expect(() => getBestHand(user_cards, community_cards)).toThrow(
+    "User should have 2 cards",
+  );
+});
+
+test("community should have 5 cards", () => {
+  // Given
+  const user_cards: Card[] = [
+    {
+      rank: Rank.Two,
+      color: Color.Spades,
+    },
+    {
+      rank: Rank.Jack,
+      color: Color.Spades,
+    },
+  ];
+
+  const community_cards: Card[] = [];
+
+  // When
+  // Then
+  expect(() => getBestHand(user_cards, community_cards)).toThrow(
+    "Community should have 5 cards",
+  );
+});
+
+test("it return 5 highest cards", () => {
+  // Given
+  const user_cards: Card[] = [
+    {
+      rank: Rank.Two,
+      color: Color.Spades,
+    },
+    {
+      rank: Rank.Jack,
+      color: Color.Spades,
+    },
+  ];
+
+  const community_cards: Card[] = [
+    {
+      rank: Rank.Six,
+      color: Color.Spades,
+    },
+    {
+      rank: Rank.Three,
+      color: Color.Spades,
+    },
+    {
+      rank: Rank.Four,
+      color: Color.Spades,
+    },
+    {
+      rank: Rank.Five,
+      color: Color.Spades,
+    },
+    {
+      rank: Rank.Seven,
+      color: Color.Spades,
+    },
+  ];
+
+  // When
+  const result = getBestHand(user_cards, community_cards);
+
+  // Then
+  expect(result.map((card) => card.rank)).toEqual([
+    Rank.Jack,
+    Rank.Seven,
+    Rank.Six,
+    Rank.Five,
+    Rank.Four,
+  ]);
+});
+
 test("it return 5 highest cards", () => {
   // Given
   const user_cards: Card[] = [
